a834c7441e52b4fda574bb1df5d82278be4ecc9a
Euler <mombergereuler@gmail.com>
2024-04-03 20:27:43 -0300

    chore: modificando regra de cors

--- src/server.js
+++ src/server.js
@@ -3,12 +3,14 @@ require('dotenv');
 
 const AppError = require('./utils/AppError');
 const express = require('express');
+const cors = require('cors');
 
 const app = express();
 
 const routes = require('./routes');
 
 app.use(express.json());
+app.use(cors());
 
 app.use(routes);
 
